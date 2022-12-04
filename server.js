c05b8cb2e6925eacbc933cdd60cb0fc11c4865f9
p513817 <p513817@gmail.com>
2022-12-04 14:34:03 +0800

    feat: extract public to root

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static('.'))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
